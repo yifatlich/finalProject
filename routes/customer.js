75888f61a8d6fe8cacbc61d1185a3a41fdf4b977
Yifat <yifatlich2001@gmail.com>
2024-10-29 22:18:35 +0200

    fixed

--- routes/customer.js
+++ routes/customer.js
@@ -2,16 +2,12 @@ const express = require("express")
 const customerController = require("../controllers/customer")
 const router = express.Router()
 
-// router.post('/', customerController.createCustomer)
 router.post('/update/:id', customerController.updateCustomer)
 router.delete('/:id', customerController.deleteCustomer)
 router.get('/', customerController.listCustomers)
 router.get('/search', customerController.renderSearchForm)
 router.get('/results', customerController.searchCustomers)
 router.get('/updateForm/:id', customerController.renderUpdateForm)
-// router.get('/searchForm', (req, res) => {
-//     res.render('customerSearch')
-// })
 router.get('/register', customerController.renderRegisterForm)
 router.post('/register', customerController.createCustomer)
 
