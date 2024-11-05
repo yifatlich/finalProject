88fd5c076c544116a1a80f80f6bf3c1f578ca63d
Yifat <yifatlich2001@gmail.com>
2024-11-05 21:05:31 +0200

    managerLogin

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -46,3 +46,27 @@ exports.renderManagerView = async (req, res) => { // Add async here
     }
 };
 
+exports.renderLoginForm = (req, res) => {
+    res.render('managerLogin')
+}
+
+exports.handleLogin = async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        const manager = await managerService.searchManagers({ username })
+        if (!manager || manager.length === 0) {
+            req.flash('error', 'Invalid username')
+            return res.redirect('/managers/login')
+        }
+
+        if (password !== manager[0].password) {
+            req.flash('error', 'Invalid password')
+            return res.redirect('/managers/login')
+        }
+        res.redirect('/')
+    } catch (error) {
+        console.error('Login error:', error)
+        res.status(500).send("An error occurred during login")
+    }
+}
