715570e6dd74509c91befa80ec4f137d273f86c3
ofir-zamorski <ofirzamorski1@gmail.com>
2024-11-05 20:38:15 +0200

    layout for all pages

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const cartRoute = require("./routes/cart");
 const addressRoute = require("./routes/address")
 const storeRoute = require("./routes/store")
 const weatherRoute = require("./routes/weather")
+const expressEjsLayouts = require('express-ejs-layouts');
+
 
 const cors = require("cors")
 const methodOverride = require('method-override')
@@ -16,10 +18,25 @@ const path = require('path')
 var app = express()
 
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.use(express.urlencoded({ extended: true }))
+
+
 app.set("view engine", "ejs")
 app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.use(expressEjsLayouts);
+
+
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 mongoose.connect('mongodb://localhost:27017/OurStore', {
     useNewUrlParser: true,
@@ -28,16 +45,6 @@ mongoose.connect('mongodb://localhost:27017/OurStore', {
 }).then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err))
 
-
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
-app.use(express.static(path.join(__dirname, 'images')));
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, 'css')));
-
 // Root URL
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -45,11 +52,13 @@ app.get('/', (req, res) => {
 
 app.use("/customers", customerRoute)
 app.use("/managers", managerRoute)
-app.use("/products", productRoute);
-app.use('/cart', cartRoute);
+app.use("/products", productRoute)
+app.use('/cart', cartRoute)
 app.use(addressRoute)
 app.use(storeRoute)
 app.use(weatherRoute)
+
+
 
 
 
