defabe6d0a7fbcdcf1d6e0d8312d7c6f3739e390
Ofir-zamorski <ofirzamorski1@gmail.com>
2024-11-08 23:47:32 +0200

    Delete routes/X.js
    wrong file

--- routes/X.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTweets } = require('../controllers/X');
-
-// נתיב לשליפת ציוצים לפי שם משתמש
-router.get('/tweets/:username', getTweets);
-
-module.exports = router;
